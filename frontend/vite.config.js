cd7658fc38a0d450879f2b18a1870da1b005ea75
Tran Nguyen Tin <62509465+eovipmak@users.noreply.github.com>
2025-11-17 23:18:17 +0700

    check

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,13 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	optimizeDeps: {
+		include: ['svelte', '@sveltejs/kit', 'vite']
+	},
 	server: {
 		host: '0.0.0.0',
 		port: 3000,
+		allowedHosts: true,
 		watch: {
 			usePolling: true
 		}
